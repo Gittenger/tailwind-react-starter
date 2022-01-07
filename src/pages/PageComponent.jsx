9863979b4f500e1c928419f8b4aced196962dbb5
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-06 20:51:11 -0500

    corrected props

--- src/pages/PageComponent.jsx
+++ src/pages/PageComponent.jsx
@@ -4,22 +4,24 @@ import PageContext from '../contexts/PageContext.js'
 import CIndex from '../components/components.index.js'
 
 const PageComponent = ({ title, Component, ...props }) => {
-	const { Layout } = CIndex
-	const { setPage } = useContext(PageContext)
+  const { Layout } = CIndex
+  const { setPage } = useContext(PageContext)
 
-	useEffect(() => {
-		document.title = `${process.env.REACT_APP_MAIN_TITLE} | ${title}`
-		setPage(title)
-	}, [title, setPage])
+  useEffect(() => {
+    document.title = `${process.env.REACT_APP_MAIN_TITLE} | ${title}`
+    setPage(title)
+  }, [title, setPage])
 
-	return (
-		<Layout>
-			<Component {...props} />
-		</Layout>
-	)
+  return (
+    <Layout>
+      <Component {...props} />
+    </Layout>
+  )
 }
 
-export const getPage = (title, component) => () =>
-	<PageComponent title={title} Component={component} />
+export const getPage =
+  (title, component, props = {}) =>
+  () =>
+    <PageComponent title={title} Component={component} props={props} />
 
 export default getPage
